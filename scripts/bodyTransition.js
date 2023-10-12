0beaf6adc732ceea1aafca3fbc6459504094cd70
Noah Veenstra <noahveenstra@icloud.com>
2023-10-12 13:20:45 -0400

    scroll trigger fiddling

---
+++ scripts/bodyTransition.js
@@ -0,0 +1,9 @@
+const body = new SplitType("h1");
+gsap.to(".line", {
+  y: 0,
+  ease: "power1.inOut",
+  stagger: 0.15,
+  delay: 0.35,
+  duration: 0.2,
+  opacity: 1,
+});
---
+++ scripts/fadeIn.js
@@ -0,0 +1,7 @@
+var tl = gsap.timeline();
+tl.to(".fadeIn", {
+  opacity: 1,
+  delay: 3,
+  duration: 0.75,
+  ease: "power1.inOut",
+});
